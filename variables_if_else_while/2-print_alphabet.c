00f80f41aaa5701523ca313d4811d9e18df35700
andrescuantico <4741@holbertonschool.com>
2022-06-19 23:28:47 -0500

    02

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -6,11 +6,11 @@
  */
 int main(void)
 {
-    int i;
-    for (i=97;i<123;i++)
-    {
+	int i;
+	for (i=97;i<123;i++)
+    	{
         putchar (i);
-    }
+    	}
 	putchar ('\n');
-    return (0);
+ 	return (0);
 }
