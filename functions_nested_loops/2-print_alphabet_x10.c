4fc3f38d9299b14233182cf3c0cebd639f55e657
andrescuantico <4741@holbertonschool.com>
2022-06-20 15:18:00 -0500

    2

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- *print_alphabet_x10 - print alphabet 10 times 
+ * print_alphabet_x10 - print alphabet 10 times 
  *
- *Return: void
+ * Return: void
  */
 
 void print_alphabet_x10(void)
