63d5b072ff8908eea4e2a7f512522d710e5f6514
andrescuantico <4741@holbertonschool.com>
2022-06-20 15:21:08 -0500

    2

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -12,11 +12,8 @@ void print_alphabet_x10(void)
 
 	for (j = 0; j < 10; j++)
 	{
-	for (i = 97; i < 123; i++)
-		_putchar(i);
+		for (i = 97; i < 123; i++)
+			_putchar(i);
 	_putchar('\n');
 	}
-
-
-
 }
