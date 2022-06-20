fe32815fc00e1627c2967862910a6966fed9794d
andrescuantico <4741@holbertonschool.com>
2022-06-20 01:18:54 -0500

    09

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/* more headers goes there */
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+		putchar(32);
+		if (i!=57)
+		putchar(44);
+	}
+	return (0);
+}
