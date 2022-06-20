80432cef44294a7fd7ede402943c3680d14f0b3b
andrescuantico <4741@holbertonschool.com>
2022-06-20 00:07:34 -0500

    04

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -12,9 +12,9 @@ int main(void)
 	int i;
 	for (i = 97 ; i < 123 ; i++)
 	{
-	if (i!=101 && i!=113)
+	if (i! = 101 && i! = 113)
 	putchar (i);
 	}  
-	putchar ('\n');
+putchar ('\n');
 	return (0);
 }
