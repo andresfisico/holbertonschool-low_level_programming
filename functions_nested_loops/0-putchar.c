fbe3a146aa8356421c9306d93d7bb10d313b87ca
andrescuantico <4741@holbertonschool.com>
2022-06-20 13:56:22 -0500

    0

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,6 +1,5 @@
 #include "main.h"
 
-/* betty style doc for function main goes there */
 /**
  * main - Entry point
  *
@@ -8,13 +7,14 @@
  */
 int main(void)
 {
-	putchar(95);
-	putchar(112);
-	putchar(117);
-	putchar(116);
-	putchar(99);
-	putchar(104);
-	putchar(97);
-	putchar(114);
+	_putchar(_);
+	_putchar(p);
+	_putchar(u);
+	_putchar(t);
+	_putchar(c);
+	_putchar(h);
+	_putchar(a);
+	_putchar(r);
+	_putchar('\n');
 	return(0);
 }
