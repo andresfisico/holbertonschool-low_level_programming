858f1c7594bf264eb8a2ab58227ef2003af5df72
andrescuantico <4741@holbertonschool.com>
2022-06-19 23:43:56 -0500

    03

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/* more headers goes there */
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    int i;
+    for (i = 97;i < 123;i++)
+    {
+        putchar (i);
+    }
+    for (i=65;i<91;i++)
+    {
+        putchar (i);
+    }
+        putchar ('\n');
+    return (0);
