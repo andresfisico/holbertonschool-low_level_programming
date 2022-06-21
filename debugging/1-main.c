8e339ed30fb48ea66e0e680f2a8503e0c9db2e19
andrescuantico <4741@holbertonschool.com>
2022-06-21 13:27:48 -0500

    1

--- debugging/1-main.c
+++ debugging/1-main.c
@@ -1,3 +1,10 @@
+#include <stdio.h>
+
+/**
+* main - causes an infinite loop
+* Return: 0
+*/
+
 int main(void)
 {
 	int i;
@@ -7,9 +14,9 @@ int main(void)
 	i = 0;
 
 	//while (i < 10)
-        {
+	{
 		putchar(i);
-        }
+	}
 
 	printf("Infinite loop avoided! \\o/\n");
 
