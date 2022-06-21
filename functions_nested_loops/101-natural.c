a54003db7eb3604b17dd6856e55c72404f787de7
andrescuantico <4741@holbertonschool.com>
2022-06-21 07:59:55 -0500

    101

---
+++ functions_nested_loops/101-natural.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main -  If we list all the natural numbers below
+ * Return: ---
+ */
+
+int main(void)
+{
+	int i, j;
+
+	for (i = 0; i < 1024; i++)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+		{
+			j += i;
+		}
+	}
+	printf("%d\n", j);
+	return (0);
+}
