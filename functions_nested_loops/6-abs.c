ca3f647a9026d2b5b783fb3293dc74bc126f8b64
andrescuantico <4741@holbertonschool.com>
2022-06-20 20:18:08 -0500

    6

--- functions_nested_loops/6-abs.c
+++ functions_nested_loops/6-abs.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- *-abs - calculate the absulute in number
+ *_abs - calculate the absulute in number
  *@i; is number calculate the absluto in number
  *Return: absolute value is number
  */
@@ -8,7 +8,7 @@ int _abs(int i)
 {
 	int a;
 
-	a = _abs(i);
+	i = _abs(i);
 
-	return (a); 
+	return (i); 
 }
