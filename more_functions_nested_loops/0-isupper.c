5c4608b934a07cced54d5900e28a0aa5fe615ddf
andrescuantico <4741@holbertonschool.com>
2022-06-21 16:39:45 -0500

    0

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _isuper  - check character.
+ * @c: check letter
+ * Retur: Value letter
+ */
+int _isupper(int c)
+{
+	char c;
+
+	if (_isupper (c ))
+		return(1);
+	else
+	return(0);
+}
+
