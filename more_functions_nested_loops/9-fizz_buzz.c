de1f7e5088b5b47d2f864fc916595ddf8e51945d
andrescuantico <4741@holbertonschool.com>
2022-06-22 14:41:29 -0500

    9

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -11,9 +11,9 @@ int main(void)
 {
 	int i;
 
-	for (i = 1; i < 101; i++)
+			for (i = 1; i < 101; i++)
 			{
-				if (i % 3 == 0 && i % 5 != 0)
+			if (i % 3 == 0 && i % 5 != 0)
 					{
 					printf("Fizz");
 					}
