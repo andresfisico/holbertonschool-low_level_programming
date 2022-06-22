0e36f99d39d0bd124fe2a55193b856068ad6f79b
andrescuantico <4741@holbertonschool.com>
2022-06-22 00:58:36 -0500

    7

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* print_diagonal - print number in 0 to 14, in a column
+*
+*
+* Return: numbers in a column
+*/
+print_diagonal(int n)
+{
+int i;
+
+ 	for (i = 0; i < n; i++)
+	{
+		_putchar('\');
+	}
+	_putchar('\n');
+}
