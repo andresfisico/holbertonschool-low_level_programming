4c369f11884e62d71db33feea413fe1874a538f0
andrescuantico <4741@holbertonschool.com>
2022-06-21 23:29:58 -0500

    3

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+ #incude "main.h" 
+ 
+/** 
+* print_numbers - print number in 0 to 9
+*
+*
+* Return: numbers 0 to 9 
+*/
+void print_numbers(void)
+{
+	for (i = 48; i < 57; i++)
+		_putchar(i)
+	_putchar('\n')
+}
