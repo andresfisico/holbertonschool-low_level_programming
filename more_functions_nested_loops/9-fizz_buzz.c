598a2a91c0860631d800b54205a1d6c206bd1f48
andrescuantico <4741@holbertonschool.com>
2022-06-22 14:32:27 -0500

    9

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -17,15 +17,15 @@ int main(void)
 					{
 					printf("Fizz");
 					}
-            		else if (i % 5 == 0 && i % 3 != 0)
+					else if (i % 5 == 0 && i % 3 != 0)
 					{
 					printf("Buzz");
 					}
-					else if (i%3 == 0 && i%5 == 0)
+					else if (i % 3 == 0 && i % 5 == 0)
 					{
 					printf("FizzBuzz");
 					}
-					else printf("%d",i);
+					else printf("%d", i);
 					if(i < 100)
 						printf(" ");
 			}
