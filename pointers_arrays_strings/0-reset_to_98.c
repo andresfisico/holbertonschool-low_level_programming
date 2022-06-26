d1cd4de33cf235c5abad981a34479342e39053f6
andrescuantico <4741@holbertonschool.com>
2022-06-26 17:02:32 -0500

    00

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * reset_to_98 - takes a pointer to an int as parameter and updates the value it points
+ * @p: pointer p
+ * Return: Always 0
+ */
+
+void reset_to_98(int *n);
+{
+	int *p; // Se declara el puntero p
+	p = &n; // Se le asigna la dirección de n
+	*p = 98; // Se actualiza el valor que apunta a 98 
+	return 0;
+}
