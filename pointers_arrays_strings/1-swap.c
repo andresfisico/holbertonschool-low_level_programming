93670e96d7b376968a82be477fdb48b3f48219f3
andrescuantico <4741@holbertonschool.com>
2022-06-27 10:45:25 -0500

    01

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,19 @@
+#include <stdioi.h>
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+void swap_int(int *a, int *b)
+{
+	int *p, *pp;
+	  
+	p=&a;
+	pp=&b;
+	  
+	p=b;
+	pp=a;
+}
+
