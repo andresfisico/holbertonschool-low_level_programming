87237dd27a54c21cc9868af60e3d54dd7c647945
andrescuantico <4741@holbertonschool.com>
2022-06-27 16:00:21 -0500

    03

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _puts - print a string
+ * @str: string
+ * Return: string.
+ */
+
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (*(s + i))
+	{
+		i++;
+		return (i);
+	}
+}
