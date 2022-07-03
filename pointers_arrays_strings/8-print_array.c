0ebfcae770b17245ea5acb1a7ace14dc2b3c3a3e
andrescuantico <4741@holbertonschool.com>
2022-07-03 16:06:01 -0500

    08

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_array - print elements to array
+ *
+ * Return: void
+ */
+void print_array(int *a, int n)
+{
+	for (i = 0; i < n; i++)
+	{
+		printf("%d, ", a[i]);
+
+	}
+
+	printf("\n");
+
+
+
+
+
+}
